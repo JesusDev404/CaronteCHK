49aedfe0f2ce56613c21f09543d113f126e9d122
Angel <soldadoazteca@live.com.mx>
2023-10-10 21:52:36 -0600

    Agregar archivos de proyecto.

---
+++ Models/ModelDB/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CaronteCHK.Models.ModelDB;
+
+public partial class User
+{
+    public int IdUser { get; set; }
+
+    public string NombreCompleto { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string Pass { get; set; } = null!;
+
+    public DateTime FechaCreacion { get; set; }
+
+    public string Rol { get; set; } = null!;
+}
